9d6b43adcac321c4da49e551879743e126dc15f0
mschmidt <kineticsparks@gmail.com>
2020-10-24 22:23:45 -0400

    Reverting to ios13 min version. Will need to fork leaf and fluent to make ios work

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "vapor",
     platforms: [
        .macOS(.v10_15),
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "Vapor", targets: ["Vapor"]),
